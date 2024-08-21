4edbe6ea3ee487e8c69f6ea314d9f96a16ef1472
Melina Zarate <melinazarate@Melinas-MacBook-Pro.local>
2024-08-20 23:32:48 -0300

    Fix styles landing

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -8,24 +8,54 @@ const Landing = () => {
           <div className="section-hero-text">
             <h2></h2>
             <h1 className="title-hero">Contabilidad realmente sencilla</h1>
-            <p>
-            Automatice la confección de libros de IVA mediante el escaneo inteligente de facturas. 
-            <br />Simplifique la gestión contable con informes detallados de clientes, optimice su tiempo con procesos automatizados, y asegure la precisión en cada registro. 
+            <p className="description-hero">
+              Automatice la confección de libros de IVA mediante el escaneo
+              inteligente de facturas.
+              <br />
+              Simplifique la gestión contable con informes detallados de
+              clientes, optimice su tiempo con procesos automatizados, y asegure
+              la precisión en cada registro.
             </p>
-
+            <div className="container-buttons-hero-checks">
+              <div className="button-child-hero-checks">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="#4DB05F"
+                  class="bi bi-check-circle-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+                <p className="p-button-check">Eficiencia asegurada</p>
+              </div>
+              <div className="button-child-hero-checks">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="#4DB05F"
+                  class="bi bi-check-circle-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+                <p className="p-button-check">Seguro y protegido</p>
+              </div>
+            </div>
             <div className="container-buttons-hero">
-              <button className="button-sign-up-hero">Registrate</button>
+              <button className="button-sign-up-hero">Regístrate</button>
               <a href="" className="link-document-api">
                 Documentación API
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="17"
                   height="17"
-                  fill="#4e56a8" 
+                  fill="#4e56a8"
                   className="bi bi-arrow-up-right"
                   viewBox="0 0 16 16"
-                  style={{marginLeft:'5px'}}
-                  
+                  style={{ marginLeft: "5px" }}
                 >
                   <path
                     fill-rule="evenodd"
