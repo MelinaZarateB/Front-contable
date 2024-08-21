522ceb6d763420d48d0c6443d5f56909fb4725d8
MelinaZarateB <melinazarate77@gmail.com>
2024-08-21 16:59:23 -0300

    Add sign-up component

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,21 +7,43 @@ const Login = () => {
     navigate("/sign-up");
   };
   return (
-    <div className="container-login-box">
-      <div className="login-box">
+    <section className="container-login-box">
+      <form className="login-box">
         <div className="login-header">
           <header>¡Bienvenido!</header>
         </div>
         <div className="input-box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#555555cd"
+            class="bi bi-person-fill"
+            viewBox="0 0 16 16"
+            className="input-icon"
+          >
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+          </svg>
           <input
             type="text"
-            class="input-field"
+            className="input-field"
             placeholder="Email"
             autocomplete="off"
             required
           />
         </div>
         <div className="input-box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="19"
+            height="19"
+            fill="#555555cd"
+            class="bi bi-lock-fill"
+            viewBox="0 0 16 16"
+            className="input-icon"
+          >
+            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2" />
+          </svg>
           <input
             type="password"
             className="input-field"
@@ -47,14 +69,14 @@ const Login = () => {
         </div>
         <div className="sign-up-link">
           <p>
-            ¿No tienes cuenta?{" "}
+            ¿No tienes una cuenta?{" "}
             <a onClick={handleButtonSignUp} className="a-link-login">
-            Regístrate acá
+              Regístrate acá
             </a>
           </p>
         </div>
-      </div>
-    </div>
+      </form>
+    </section>
   );
 };
 
