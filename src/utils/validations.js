f36d46dfed9ac0e1644cc77a512d3b27eb91ec2f
MelinaZarateB <melinazarate77@gmail.com>
2024-09-01 21:38:57 -0300

    Fix import validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,5 +1,6 @@
 const regexName = /^[^\d]+$/;
-const regexPassword = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_+\-]).{8,}$/;
+const regexPassword =
+  /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_+\-]).{8,}$/;
 const regexEmail = /^[^@\s]+@[^@\s]+\.[cC][oO][mM]$/;
 
 const validations = (inputs) => {
@@ -13,30 +14,43 @@ const validations = (inputs) => {
     }
   }
 
-  if(inputs.phone.trim() === "") {
+  if (inputs.phone.trim() === "") {
     errors.phone = "Campo requerido";
   }
 
-  if(inputs.address.trim() === "") {
+  if (inputs.address.trim() === "") {
     errors.address = "Campo requerido";
   }
-  if(inputs.city.trim() === "") {
+  if (inputs.city.trim() === "") {
     errors.city = "Campo requerido";
   }
-  if(!regexEmail.test(inputs.email) || inputs.email.trim() === "") {
-    errors.email = 'Campo requerido';
+  if (!regexEmail.test(inputs.email) || inputs.email.trim() === "") {
+    errors.email = "Campo requerido";
   }
-  if(!regexPassword.test(inputs.password)) {
+  if (!regexPassword.test(inputs.password)) {
     errors.password = "Campo requerido";
   }
-  return errors
-
+  return errors;
 };
-const validationRestorePassword = (input) => {
+const validationsLogin = (inputs) => {
   const errors = {};
-  if(!regexEmail.test(input.email) || input.email.trim() === "") {
-    errors.email = 'Campo requerido';
+
+  if (inputs.email.trim() === "") {
+    errors.email = "Campo requerido";
+  } else if (!regexEmail.test(inputs.email)) {
+    errors.email = "Email inválido";
+  }
+  if (inputs.password.trim() === "") {
+    errors.password = "Campo requerido";
   }
   return errors;
-}
-export {validations, validationRestorePassword};
+};
+
+const validationRestorePassword = (input) => {
+  const errors = {};
+  if (!regexEmail.test(input.email) || input.email.trim() === "") {
+    errors.email = "Campo requerido";
+  }
+  return errors;
+};
+export { validations, validationsLogin, validationRestorePassword };
